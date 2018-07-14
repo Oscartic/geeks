586c296957b61e70535f4c44836678e164301b70
Oscar René Ballesteros Plaza <oscartic@gmail.com>
2018-07-13 21:08:21 -0400

    Add locale configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
+  # set_variant detecta el dispositivo por el cual se accede y asigna el template correspondiente:
   before_action :set_variant
+  # set_locale detecta el lugar de acceso para mostrar la traduccion conrrespondiente
+  before_action :set_locale
+
 =begin
   def set_variant
     return unless params[:variant].in?(%w(phone tablet desktop))
@@ -11,5 +15,9 @@ class ApplicationController < ActionController::Base
     # request.variant = :phone if request.user_agent.include?('iPhone')
     # o con la gema browser
     request.variant = :phone if browser.device.mobile?
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale]
   end
 end
