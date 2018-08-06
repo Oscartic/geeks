d03c7b249312a715c70c266ef6ef39fbeb838a82
Oscar René Ballesteros Plaza <oscartic@gmail.com>
2018-08-06 17:53:58 -0400

    Maximus longituds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,16 @@ authors.each {|author| FactoryBot.create_list(:book, Random.rand(0..6) * Random.
 puts "+++++> ------- Seed done. Authors #{Author.count}, Books: #{Book.count}"
 Country.find_or_create_by(code: 'es_ES', name: 'España');
 Country.find_or_create_by(code: 'en_US', name: 'USA');
+
+email= "maximuslongitudxxxx@maximuslongitudxxx.com.co"
+unless Author.find_by(email: email)
+  maximuslongituds = FactoryBot.create(
+                   :author,
+                   first_name: 'Maximus longituds froilan de maximiliano real',
+                   last_name: 'Cara de palo, estronimostancio del perpetuo cansodoro',
+                   email: email,
+                   section: 'fijo 22224587, llamar solo en las mayanaos, Movil +5987145531',
+                   shelf: 'Bancuber_casildense de la ciudad de monaco, bahamas',
+                   order: 'https://medium.com/@codenode/10-tips-for-eager-loading-to-avoid-n-1-queries-in-rails-2bad54456a3f'
+            )
+end
